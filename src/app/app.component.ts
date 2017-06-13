bef73c1d6bd1be79467cadffd08b693efccf4092
WuJun <wujun19890209@gmail.com>
2017-06-13 14:34:10 +0800

    feat: livequery demo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RxAVObject, RxAVQuery, RxAVLiveQuery } from 'rx-lean-js-core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  doing: Array<RxAVObject> = [];
+  done: Array<RxAVObject> = [];
+  ngOnInit(): void {
+
+    let doingQuery = new RxAVQuery('TodoLiveQuery');
+    doingQuery.equalTo('tag', 'LiveQuery_demo');
+    doingQuery.equalTo('state', 'doing');
+
+    doingQuery.find().subscribe(data => {
+      data.forEach(todo => {
+        this.doing.push(todo);
+      });
+    });
+
+    let doneQuery = new RxAVQuery('TodoLiveQuery');
+    doneQuery.equalTo('tag', 'LiveQuery_demo');
+    doneQuery.equalTo('state', 'done');
+
+    doneQuery.find().subscribe(data => {
+      data.forEach(todo => {
+        this.done.push(todo);
+      });
+    });
+
+    let doingSubscription = doingQuery.subscribe();
+    let doneSubscription = doneQuery.subscribe();
+
+    doingSubscription.flatMap(subs => {
+      subs.on.subscribe(pushData => {
+        if (pushData.scope == 'create' || pushData.scope == 'enter') {
+          this.doing.push(pushData.object);
+        } else if (pushData.scope == 'leave' || pushData.scope == 'delete') {
+          this.unassign(this.doing, pushData.object, 'doing');
+        }
+      });
+      return doneSubscription;
+    }).subscribe(subs => {
+      subs.on.subscribe(pushData => {
+        if (pushData.scope == 'create' || pushData.scope == 'enter') {
+          this.done.push(pushData.object);
+        } else if (pushData.scope == 'leave' || pushData.scope == 'delete') {
+          this.unassign(this.done, pushData.object, 'doing');
+        }
+      });
+    });
+  }
+
+  checked(todo: RxAVObject) {
+    todo.set('state', 'done');
+    todo.save().subscribe(success => {
+
+    });
+  }
+  unchecked(todo: RxAVObject) {
+    todo.set('state', 'doing');
+    todo.save().subscribe(success => {
+
+    });
+  }
+
+  private unassign(todos: Array<RxAVObject>, todo: RxAVObject, state: string) {
+    let tempTodo = todos.find(item => item.objectId == todo.objectId);
+    if (tempTodo) {
+      let index = todos.indexOf(tempTodo);
+      if (index > -1) {
+        todos.splice(index, 1);
+      }
+    }
+  }
+  title = '';
+
+  add() {
+    let todo = new RxAVObject('TodoLiveQuery');
+    todo.set('tag', 'LiveQuery_demo');
+    todo.set('title', this.title);
+    todo.set('state', 'doing');
+    todo.save().subscribe(saved => {
+
+      console.log('saved', saved);
+      this.title = '';
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,32 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+import { MdToolbarModule } from '@angular/material';
+import { MdCheckboxModule } from '@angular/material';
+import { MdIconModule } from '@angular/material';
+import { MdButtonModule } from '@angular/material';
+import { MdMenuModule } from '@angular/material';
+import { MdTabsModule } from '@angular/material';
+import { MdListModule } from '@angular/material';
+import { MdInputModule } from '@angular/material';
+import { MdCardModule } from '@angular/material';
+import { RxAVClient, RxAVApp } from 'rx-lean-js-core';
+import { ngRxLeanCloud } from 'rx-lean-angular';
+
+let app = new RxAVApp({
+  appId: `Dfvaqzrmz1x4Yos6v4DWdpwg-gzGzoHsz`,
+  appKey: `I4XueooVQ4r1fUhX4bmaGpNM`
+});
+
+RxAVClient.init({
+  plugins: {
+    websocket: ngRxLeanCloud.ngWebSocketClient
+  }
+}).add(app);
 
 @NgModule({
   declarations: [
@@ -12,7 +38,18 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MdCheckboxModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdButtonModule,
+    MdMenuModule,
+    MdTabsModule,
+    MdListModule,
+    MdInputModule,
+    MdCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
